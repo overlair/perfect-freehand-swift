3622fb1e20403d16448da1c4b2dad64a715f4443
overlair <overlair@gmail.com>
2025-01-27 22:52:17 -0800

    changed Package name

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "perfect-freehand-swift",
+    name: "PerfectFreehand",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "perfect-freehand-swift",
-            targets: ["perfect-freehand-swift"]),
+            name: "PerfectFreehand",
+            targets: ["PerfectFreehand"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "perfect-freehand-swift"),
+            name: "PerfectFreehand"),
         .testTarget(
             name: "perfect-freehand-swiftTests",
-            dependencies: ["perfect-freehand-swift"]
+            dependencies: ["PerfectFreehand"]
         ),
     ]
 )
